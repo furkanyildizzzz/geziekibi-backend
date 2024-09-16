807ee26cccf19f161ce0d2b5a5b4818276656319
furkotikoo <furkanyildiz47@gmail.com>
2024-09-16 13:31:28 +0300

    register controller

---
+++ src/orm/entities/users/User.ts
@@ -0,0 +1,60 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Language, Role } from './types';
+import bcrypt from 'bcryptjs';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    nullable: true,
+    unique: true,
+  })
+  username;
+
+  @Column({
+    nullable: true,
+  })
+  name: string;
+
+  @Column({
+    default: 'STANDART' as Role,
+    length: 30,
+  })
+  role: string;
+
+  @Column({
+    default: 'en-US' as Language,
+    length: 15,
+  })
+  language: string;
+
+  @Column()
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Column()
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  setLanguage(language: Language) {
+    this.language = language;
+  }
+
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
+
+  checkIfPasswordMatch(unencryptedPassword: string) {
+    return bcrypt.compareSync(unencryptedPassword, this.password);
+  }
+}
--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,3 +1,4 @@
+import { register } from 'controllers/auth/register';
 import { Router } from 'express';
 import { checkJwt } from 'middleware/checkJwt';
 import { validatorChangePassword, validatorLogin, validatorRegister } from 'middleware/validation/auth';
@@ -5,7 +6,7 @@ import { validatorChangePassword, validatorLogin, validatorRegister } from 'midd
 const router = Router();
 
 router.post('/login', [validatorLogin]);
-router.post('/register', [validatorRegister]);
+router.post('/register', [validatorRegister], register);
 router.post('/change-password', [checkJwt, validatorChangePassword]);
 
 export default router;
