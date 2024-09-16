2e7cb4b34812ba6e4553f179c1b679ff46af8431
furkotikoo <furkanyildiz47@gmail.com>
2024-09-16 21:47:05 +0300

    feat(auth): changePassword controller created

--- src/middleware/validation/auth/validatorChangePassword.ts
+++ src/middleware/validation/auth/validatorChangePassword.ts
@@ -5,20 +5,20 @@ import { ErrorValidation } from 'utils/response/custom-error/types';
 import validator from 'validator';
 
 export const validatorChangePassword = (req: Request, res: Response, next: NextFunction) => {
-  let { password, newPassword, passwordConfirm } = req.body;
+  let { password, passwordNew, passwordConfirm } = req.body;
   const errorsValidation: ErrorValidation[] = [];
 
   password = !password ? '' : password;
-  newPassword = !newPassword ? '' : newPassword;
+  passwordNew = !passwordNew ? '' : passwordNew;
   passwordConfirm = !passwordConfirm ? '' : passwordConfirm;
 
   if (validator.isEmpty(password)) errorsValidation.push({ password: 'Password field is required' });
-  if (validator.isEmpty(newPassword)) errorsValidation.push({ newPassword: 'New Password field is required' });
-  if (!validator.isLength(newPassword, { min: ConstsUser.PASSWORD_MIN_CHAR }))
-    errorsValidation.push({ newPassword: `New Password must be at least ${ConstsUser.PASSWORD_MIN_CHAR} charecters` });
+  if (validator.isEmpty(passwordNew)) errorsValidation.push({ passwordNew: 'New Password field is required' });
+  if (!validator.isLength(passwordNew, { min: ConstsUser.PASSWORD_MIN_CHAR }))
+    errorsValidation.push({ passwordNew: `New Password must be at least ${ConstsUser.PASSWORD_MIN_CHAR} charecters` });
   if (validator.isEmpty(passwordConfirm))
     errorsValidation.push({ passwordConfirm: 'Confirm Password field is required' });
-  if (!validator.equals(newPassword, passwordConfirm))
+  if (!validator.equals(passwordNew, passwordConfirm))
     errorsValidation.push({ passwordConfirm: 'Confirm Password does not match' });
 
   if (errorsValidation.length !== 0) {
--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,4 +1,5 @@
 import { login, register } from 'controllers/auth';
+import { changePassword } from 'controllers/auth/changePassword';
 import { Router } from 'express';
 import { checkJwt } from 'middleware/checkJwt';
 import { validatorChangePassword, validatorLogin, validatorRegister } from 'middleware/validation/auth';
@@ -7,6 +8,6 @@ const router = Router();
 
 router.post('/login', [validatorLogin], login);
 router.post('/register', [validatorRegister], register);
-router.post('/change-password', [checkJwt, validatorChangePassword]);
+router.post('/change-password', [checkJwt, validatorChangePassword], changePassword);
 
 export default router;
