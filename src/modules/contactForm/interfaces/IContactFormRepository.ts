a16b9fb80f3ebe3b4be84ed8efda16ea9ca3f57d
Furkan Yıldız <furkanyildiz47@gmail.com>
2025-03-03 05:44:25 +0300

    contactForm transferred to baserepositorty

--- src/modules/contactForm/interfaces/IContactFormRepository.ts
+++ src/modules/contactForm/interfaces/IContactFormRepository.ts
@@ -1,9 +1,10 @@
 import { ContactForm } from 'orm/entities/contactForm/ContactForm';
+import { IBaseRepository } from 'shared/interfaces/IBaseRepository';
 
-export interface IContactFormRepository {
-  getAll(): Promise<ContactForm[] | void>;
-  getById(id: number): Promise<ContactForm | void>;
-  update(id: number, tag: ContactForm): Promise<ContactForm>;
-  delete(id: number): Promise<void>;
-  deleteMultiple(ids: number[]): Promise<void>;
+export interface IContactFormRepository extends IBaseRepository<ContactForm> {
+  // getAll(): Promise<ContactForm[] | void>;
+  // getById(id: number): Promise<ContactForm | void>;
+  // update(id: number, tag: ContactForm): Promise<ContactForm>;
+  // delete(id: number): Promise<void>;
+  // deleteMultiple(ids: number[]): Promise<void>;
 }
--- src/modules/contactForm/repository/ContactFormRepository.ts
+++ src/modules/contactForm/repository/ContactFormRepository.ts
@@ -4,63 +4,66 @@ import { ContactForm } from 'orm/entities/contactForm/ContactForm';
 import { InternalServerErrorException } from 'shared/errors/allException';
 import { INTERFACE_TYPE } from 'core/types';
 import { UnitOfWork } from 'unitOfWork/unitOfWork';
+import { BaseRepository } from 'shared/repositories/BaseRepository';
 
 @injectable()
-export class ContactFormRepository implements IContactFormRepository {
-  constructor(@inject(INTERFACE_TYPE.UnitOfWork) private readonly unitOfWork: UnitOfWork) {}
-
-  async getAll(): Promise<ContactForm[] | void> {
-    try {
-      const repo = await this.unitOfWork.getRepository(ContactForm);
-      const contactForms = await repo.find();
-      if (contactForms) return contactForms as ContactForm[];
-    } catch (error) {
-      throw new InternalServerErrorException(`${error.message}`);
-    }
+export class ContactFormRepository extends BaseRepository<ContactForm> implements IContactFormRepository {
+  constructor(@inject(INTERFACE_TYPE.UnitOfWork) unitOfWork: UnitOfWork) {
+    super(unitOfWork, ContactForm)
   }
 
-  async getById(id: number): Promise<ContactForm | void> {
-    try {
-      const repo = await this.unitOfWork.getRepository(ContactForm);
-      const contactForm = await repo.findOne({ where: { id: id } });
-      if (contactForm) return contactForm as ContactForm;
-    } catch (error) {
-      throw new InternalServerErrorException(`${error.message}`);
-    }
-  }
+  // async getAll(): Promise<ContactForm[] | void> {
+  //   try {
+  //     const repo = await this.unitOfWork.getRepository(ContactForm);
+  //     const contactForms = await repo.find();
+  //     if (contactForms) return contactForms as ContactForm[];
+  //   } catch (error) {
+  //     throw new InternalServerErrorException(`${error.message}`);
+  //   }
+  // }
 
-  async update(id: number, contactForm: ContactForm): Promise<ContactForm> {
-    try {
-      await this.unitOfWork.startTransaction();
-      await (await this.unitOfWork.getRepository(ContactForm)).update(id, contactForm);
-      await this.unitOfWork.commitTransaction();
-      return contactForm;
-    } catch (error) {
-      await this.unitOfWork.rollbackTransaction();
-      throw new InternalServerErrorException(error.message);
-    }
-  }
+  // async getById(id: number): Promise<ContactForm | void> {
+  //   try {
+  //     const repo = await this.unitOfWork.getRepository(ContactForm);
+  //     const contactForm = await repo.findOne({ where: { id: id } });
+  //     if (contactForm) return contactForm as ContactForm;
+  //   } catch (error) {
+  //     throw new InternalServerErrorException(`${error.message}`);
+  //   }
+  // }
 
-  async delete(id: number): Promise<void> {
-    try {
-      await this.unitOfWork.startTransaction();
-      await (await this.unitOfWork.getRepository(ContactForm)).delete(id);
-      await this.unitOfWork.commitTransaction();
-    } catch (error) {
-      await this.unitOfWork.rollbackTransaction();
-      throw new InternalServerErrorException(error.message);
-    }
-  }
+  // async update(id: number, contactForm: ContactForm): Promise<ContactForm> {
+  //   try {
+  //     await this.unitOfWork.startTransaction();
+  //     await (await this.unitOfWork.getRepository(ContactForm)).update(id, contactForm);
+  //     await this.unitOfWork.commitTransaction();
+  //     return contactForm;
+  //   } catch (error) {
+  //     await this.unitOfWork.rollbackTransaction();
+  //     throw new InternalServerErrorException(error.message);
+  //   }
+  // }
 
-  async deleteMultiple(ids: number[]): Promise<void> {
-    try {
-      await this.unitOfWork.startTransaction();
-      const repo = await this.unitOfWork.getRepository(ContactForm);
-      ids.forEach(async (id) => await repo.delete(id));
-      await this.unitOfWork.commitTransaction();
-    } catch (error) {
-      await this.unitOfWork.rollbackTransaction();
-      throw new InternalServerErrorException(error.message);
-    }
-  }
+  // async delete(id: number): Promise<void> {
+  //   try {
+  //     await this.unitOfWork.startTransaction();
+  //     await (await this.unitOfWork.getRepository(ContactForm)).delete(id);
+  //     await this.unitOfWork.commitTransaction();
+  //   } catch (error) {
+  //     await this.unitOfWork.rollbackTransaction();
+  //     throw new InternalServerErrorException(error.message);
+  //   }
+  // }
+
+  // async deleteMultiple(ids: number[]): Promise<void> {
+  //   try {
+  //     await this.unitOfWork.startTransaction();
+  //     const repo = await this.unitOfWork.getRepository(ContactForm);
+  //     ids.forEach(async (id) => await repo.delete(id));
+  //     await this.unitOfWork.commitTransaction();
+  //   } catch (error) {
+  //     await this.unitOfWork.rollbackTransaction();
+  //     throw new InternalServerErrorException(error.message);
+  //   }
+  // }
 }
